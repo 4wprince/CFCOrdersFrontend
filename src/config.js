dc4a55f7eae978220966ab58cd51803a32802280
FL Man <4wprince@gmail.com>
2026-03-24 10:16:29 -0400

    remove password
    Removed commented-out APP_PASSWORD and redundant API_URL line.

--- src/config.js
+++ src/config.js
@@ -8,6 +8,3 @@
 export const API_URL = 'https://cfcorderbackend-sandbox.onrender.com'
 // Production: 'https://cfc-backend-b83s.onrender.com'
 // Sandbox:    'https://cfcorderbackend-sandbox.onrender.com'
-
-// App password
-export const APP_PASSWORD = 'cfc2025'
